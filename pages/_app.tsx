3082bec80eaa680539bc80946ab18fc60e798a85
Nitansh Shankar <167768158+BIJJUDAMA@users.noreply.github.com>
2026-02-23 16:18:59 +0530

    feat: Google analytics script added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import { GoogleAnalytics } from '@next/third-parties/google';
+import Script from 'next/script';
 import '../src/styles/styles.css';
 import { useEffect } from 'react';
 import Modal from 'react-modal';
@@ -9,10 +9,26 @@ export default function InCTFJrWebApp({ Component, pageProps }) {
     Modal.setAppElement('#__next');
   }, []);
 
+  const gaId = process.env.NEXT_PUBLIC_GA_ID;
+
   return (
     <>
       <Component {...pageProps} />
-      <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GA_ID} />
+
+      {/* Google Analytics */}
+      {gaId && (
+        <>
+          <Script src={`https://www.googletagmanager.com/gtag/js?id=${gaId}`} strategy="afterInteractive" />
+          <Script id="google-analytics" strategy="afterInteractive">
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${gaId}');
+            `}
+          </Script>
+        </>
+      )}
     </>
   );
 }
