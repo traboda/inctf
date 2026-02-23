f44fe8faba117a25c34d75f1a529502e7603f976
Nitansh Shankar <167768158+BIJJUDAMA@users.noreply.github.com>
2026-02-23 22:35:26 +0530

    feat: google analytics with page routing

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,39 @@
 import Script from 'next/script';
+import { useRouter } from 'next/router';
 import '../src/styles/styles.css';
 import { useEffect } from 'react';
 import Modal from 'react-modal';
 
+import { pageview } from '../src/lib/analytics';
+
 export default function InCTFJrWebApp({ Component, pageProps }) {
+  const router = useRouter();
+
   useEffect(() => {
     // Set Modal app element after mount to avoid hydration errors
     Modal.setAppElement('#__next');
   }, []);
 
   const gaId = process.env.NEXT_PUBLIC_GA_ID;
+
+  useEffect(() => {
+    if (!gaId) return;
+
+    // Send initial pageview since we disabled auto-send
+    if (typeof window !== 'undefined') {
+      pageview(window.location.pathname, gaId);
+    }
+
+    const handleRouteChange = (url: string) => {
+      pageview(url, gaId);
+    };
+
+    router.events.on('routeChangeComplete', handleRouteChange);
+
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events, gaId]);
 
   return (
     <>
@@ -24,7 +48,9 @@ export default function InCTFJrWebApp({ Component, pageProps }) {
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
-              gtag('config', '${gaId}');
+              gtag('config', '${gaId}', {
+                send_page_view: false,
+              });
             `}
           </Script>
         </>
