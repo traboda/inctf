2d78b4e6831b1cac53d295b4355b59d771dd14c4
Nitansh Shankar <167768158+BIJJUDAMA@users.noreply.github.com>
2026-03-01 18:41:27 +0530

    feat: Using inbuilt Google Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import '../src/styles/styles.css';
 import { ReactNode } from 'react';
 import Script from 'next/script';
+import { GoogleAnalytics } from '@next/third-parties/google';
 
 export const metadata = {
     title: 'InCTF 2026 | India\'s First Ethical Hacking Contest',
@@ -16,21 +17,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
                 {children}
 
                 {/* Google Analytics */}
-                {gaId && (
-                    <>
-                        <Script src={`https://www.googletagmanager.com/gtag/js?id=${gaId}`} strategy="afterInteractive" />
-                        <Script id="google-analytics" strategy="afterInteractive">
-                            {`
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-                gtag('config', '${gaId}', {
-                  send_page_view: false,
-                });
-              `}
-                        </Script>
-                    </>
-                )}
+                {gaId && <GoogleAnalytics gaId={gaId} />}
 
                 {/* InCTF SDK */}
                 <Script src="https://play.inctf.in/sdk.js" strategy="afterInteractive" />
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,9 @@
-import Script from 'next/script';
 import { useRouter } from 'next/router';
 import '../src/styles/styles.css';
 import { useEffect } from 'react';
 import Modal from 'react-modal';
-
-import { pageview } from '../src/lib/analytics';
+import Script from 'next/script';
+import { GoogleAnalytics } from '@next/third-parties/google';
 
 export default function InCTFJrWebApp({ Component, pageProps }) {
   const router = useRouter();
@@ -16,45 +15,12 @@ export default function InCTFJrWebApp({ Component, pageProps }) {
 
   const gaId = process.env.NEXT_PUBLIC_GA_ID;
 
-  useEffect(() => {
-    if (!gaId) return;
-
-    // Send initial pageview since we disabled auto-send
-    if (typeof window !== 'undefined') {
-      pageview(window.location.pathname, gaId);
-    }
-
-    const handleRouteChange = (url: string) => {
-      pageview(url, gaId);
-    };
-
-    router.events.on('routeChangeComplete', handleRouteChange);
-
-    return () => {
-      router.events.off('routeChangeComplete', handleRouteChange);
-    };
-  }, [router.events, gaId]);
-
   return (
     <>
       <Component {...pageProps} />
 
       {/* Google Analytics */}
-      {gaId && (
-        <>
-          <Script src={`https://www.googletagmanager.com/gtag/js?id=${gaId}`} strategy="afterInteractive" />
-          <Script id="google-analytics" strategy="afterInteractive">
-            {`
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', '${gaId}', {
-                send_page_view: false,
-              });
-            `}
-          </Script>
-        </>
-      )}
+      {gaId && <GoogleAnalytics gaId={gaId} />}
 
       {/* InCTF SDK */}
       <Script src="https://play.inctf.in/sdk.js" strategy="afterInteractive" />
